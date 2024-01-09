245b5e433d31fb1b0d163bed3d7ebc6e38f74c0c
chrisytaylor <155927068+chrisytaylor@users.noreply.github.com>
2024-01-08 20:35:14 -0600

    Add files via upload

--- propellant_cost.py
+++ propellant_cost.py
@@ -1,11 +1,17 @@
 # propellant_cost
-# by Chris Y. Taylor 01/02/2024
+# by Chris Y. Taylor
 # calculates specific cost of propellant based on fuel
 # and oxidizer cost and mixture ratio
 
-fuel_cost = float(input("What is specific fuel cost? "))
-oxidizer_cost = float(input("What is specific oxidizer cost? "))
-mixture_ratio = float(input("What is the mixture ratio (oxidizer mass/fuel mass)?"))
+bad_input = True  #not really bad yet, just nothing entered at this point
+while bad_input == True:
+    fuel_cost = float(input("What is specific fuel cost? "))
+    oxidizer_cost = float(input("What is specific oxidizer cost? "))
+    mixture_ratio = float(input("What is the mixture ratio (oxidizer mass/fuel mass)? "))
+    if fuel_cost >= 0 and oxidizer_cost >= 0 and mixture_ratio > 0:
+        bad_input = False
+    else:
+        print ("Mixture ratio must be a positive number.  Costs must be positive or zero.")
 propellant_cost = (oxidizer_cost*(mixture_ratio/(mixture_ratio+1)))+(fuel_cost*(1/(mixture_ratio+1)))
 print ("The specific propellant cost is:")
 print (propellant_cost)
